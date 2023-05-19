ac88aca1865a7e2744b9f1c80964303fe0cade88
Bryan Cruz <bryanerickcruznina@gmail.com>
2023-05-19 11:08:24 -0400

    michi

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3003;
+  await app.listen(port);
+  console.log ("todo nais " + port);
 }
 bootstrap();
