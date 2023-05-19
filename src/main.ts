b5bb8ca1bc57a88d35614e3f5fe4f1defb108ec9
Bryan Cruz <bryanerickcruznina@gmail.com>
2023-05-18 21:52:36 -0400

    michsss

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const allowedOrigins = ['http://localhost:3000', 'https://testrailway-production-cf5a.up.railway.app'];
-
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    optionsSuccessStatus: 200
-  }));
-
+  const app = await NestFactory.create(AppModule,{cors:true});
   await app.listen(3000);
 }
 bootstrap();
